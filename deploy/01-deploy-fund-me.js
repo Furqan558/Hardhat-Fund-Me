2053b204b96fea8d7c855995310dac74d32450c5
Furqan <furqanhameed558@gmail.com>
2022-08-14 00:29:05 +0500

    completed lesson 7

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -6,6 +6,8 @@
 
 const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 const { network } = require("hardhat")
+const { verify } = require("../utils/verify")
+
 // module.exports.default = deployFunc
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
@@ -21,11 +23,20 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     }
 
     //when going for localhost or working with hardhat we use mocking
+    const args = [ethUsdPriceFeedAddress]
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [ethUsdPriceFeedAddress], //put price feed address
+        args: args, //put price feed address
         log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
     })
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args)
+    }
     log("------------------------------------------------")
 }
 
